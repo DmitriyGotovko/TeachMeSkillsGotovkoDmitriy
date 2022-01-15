eeb8ceb11ffb70ddf4d4713c38ac2be8a88d7d79
Грачёв Никита Станиславович (4056057) <n.grachev@dev.vtb.ru>
2022-01-15 17:18:44 +0300

    Lesson2 HW

---
+++ src/Lesson2/Task3.java
@@ -0,0 +1,15 @@
+package Lesson2;
+
+public class Task3 {
+
+    public static void main(String[] args){
+
+        int squareSide = 5;
+        String symbol = "o";
+        System.out.println(symbol + symbol + symbol + symbol + symbol);
+        System.out.println(symbol + symbol + symbol + symbol + symbol);
+        System.out.println(symbol+symbol+symbol+symbol+symbol);
+        System.out.println(symbol+symbol+symbol+symbol+symbol);
+        System.out.println(symbol+symbol+symbol+symbol+symbol);
+    }
+}
